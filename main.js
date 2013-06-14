769cd82e3a2ed82654112c309cce9370d9ff5239
jankuca <jan@jankuca.com>
2013-06-15 00:04:48 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var store = require('nstore');
+
+var home_dirname = process.env['HOME'];
+var username = process.env['SERVERLICIOUS_USER'];
+var command = process.env['SSH_ORIGINAL_COMMAND'];
+
+var args = command.split(/\s+/);
+var executable = args.shift();
+
+var users;
+try {
+  users = store(path.join(home_dirname, '.serverlicious', 'users.db'));
+} catch (err) {
+  throw new Error('Failed to open the user database');
+}
+
+users.get(username, function (err, user) {
+  if (err) {
+    throw new Error('No such user "' + username + '"');
+  }
+
+  process.stdout.write('Continue handling command: %s\n', command);
+
+  switch (executable) {
+  case 'git':
+
+  }
+});
